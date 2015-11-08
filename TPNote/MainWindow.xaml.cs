7a8dab83431019111be035360384e8e1b8a6f3a9
Baptiste <baptiste@lyondev.fr>
2015-11-08 20:56:23 +0100

    amélioration de la circulation

--- TPNote/MainWindow.xaml.cs
+++ TPNote/MainWindow.xaml.cs
@@ -31,6 +31,7 @@ namespace TPNote
         private Ellipse feuD;
         private Boolean feuGActif;
         private int secondeFeu;
+        private int secondeCanvas;
 
         public MainWindow()
         {
@@ -110,41 +111,7 @@ namespace TPNote
                     feuH.Fill = Brushes.GreenYellow;
                     feuB.Fill = Brushes.GreenYellow;
                 }
-            }
-            
-            //if (feuB.Fill == Brushes.GreenYellow && secondeFeu == 7)
-            //{
-            //    feuB.Fill = Brushes.Orange;
-            //    feuH.Fill = Brushes.Orange;
-            //}
-            //if (feuB.Fill == Brushes.Orange && secondeFeu == 10)
-            //{
-            //    feuB.Fill = Brushes.Red;
-            //    feuH.Fill = Brushes.Red;
-            //}
-            //if (feuB.Fill == Brushes.Red && secondeFeu == 13)
-            //{
-            //    secondeFeu = 0;
-            //    feuG.Fill = Brushes.GreenYellow;
-            //    feuD.Fill = Brushes.GreenYellow;
-            //}
-
-            //if (feuG.Fill == Brushes.GreenYellow && secondeFeu == 7)
-            //{
-            //    feuG.Fill = Brushes.Orange;
-            //    feuD.Fill = Brushes.Orange;
-            //}
-            //if (feuG.Fill == Brushes.Orange && secondeFeu == 10)
-            //{
-            //    feuG.Fill = Brushes.Red;
-            //    feuD.Fill = Brushes.Red;
-            //}
-            //if (feuG.Fill == Brushes.Red && secondeFeu == 13)
-            //{
-            //    secondeFeu = 0;
-            //    feuH.Fill = Brushes.GreenYellow;
-            //    feuB.Fill = Brushes.GreenYellow;
-            //}         
+            }      
         }
 
         private void FrmPrinc_Loaded(object sender, RoutedEventArgs e)
@@ -156,14 +123,25 @@ namespace TPNote
             dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += dispatcherTimer_Tick;
             dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 10);
-            //voitureList.Add(new VoitureAgent);
             dispatcherTimer.Start();
             InitialisationDesFeux();
+            feuH.Fill = Brushes.GreenYellow;
+            feuB.Fill = Brushes.GreenYellow;
+            feuG.Fill = Brushes.Red;
+            feuD.Fill = Brushes.Red;
+            feuGActif = false;
+            feuTimer.Start();
         }
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-
+            secondeCanvas++;
+            if(secondeCanvas % 100 == 0){
+                VoitureAgent uneVoiture = new VoitureAgent();
+                //Console.WriteLine(uneVoiture.Direction);
+                voitureList.Add(uneVoiture);
+                DrawVoiture(uneVoiture);
+            }
             updateVoitures();
             drawVoitures();
         }
@@ -178,133 +156,175 @@ namespace TPNote
             routeCanvas.Children.Add(body);
             Canvas.SetLeft(body, voiture.CoordonneesApparition[0]);
             Canvas.SetTop(body, voiture.CoordonneesApparition[1]);
+            
+
+
+            /*Rectangle body = new Rectangle();
+            body.Stroke = Brushes.Chocolate;
+            body.Fill = Brushes.Coral;
+            body.Width = 250;// voiture.Width;
+            body.Height = 40;//voiture.Height;
+            routeCanvas.Children.Add(body);
+            Canvas.SetLeft(body, 390);
+            Canvas.SetTop(body, 300);*/
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
 
             VoitureAgent uneVoiture = new VoitureAgent();
-            Console.WriteLine(uneVoiture.Direction);
+            //Console.WriteLine(uneVoiture.Direction);
             voitureList.Add(uneVoiture);
             DrawVoiture(uneVoiture);
+        }
+
+        private Boolean isStopped(VoitureAgent voiture)
+        {
+            double distance = 0;
+            string apparition = voiture.apparitionToString();
+            int left = voiture.CoordonneesApparition[0];
+            int top = voiture.CoordonneesApparition[1];
+
+            // on vérifie si la voiture est au feu rouge qui la concerne
+            if (apparition == "left" && feuG.Fill == Brushes.Red && left == 244)
+                return true;
+
+            if (apparition == "right" && feuD.Fill == Brushes.Red && left == 424)
+                return true;
+
+            if (apparition == "bot" && feuB.Fill == Brushes.Red && top == 424)
+                return true;
+
+            if (apparition == "top" && feuH.Fill == Brushes.Red && top == 244)
+                return true;
+            //
+
+            foreach (VoitureAgent uneVoiture in voitureList)
+            {
+                if (uneVoiture != voiture) 
+                {
+                    //on va déterminer si une voiture devant est arrêtée
+                    if (uneVoiture.Stopped && !uneVoiture.Turned && uneVoiture.apparitionToString() == voiture.apparitionToString()){
+
+                        if(uneVoiture.apparitionToString() == "left")
+                            distance = uneVoiture.CoordonneesApparition[0] - (voiture.CoordonneesApparition[0] + voiture.Width);
+                        else if(uneVoiture.apparitionToString() == "right")
+                            distance = voiture.CoordonneesApparition[0] - (uneVoiture.CoordonneesApparition[0] + uneVoiture.Width);
+                        else if (uneVoiture.apparitionToString() == "bot")
+                            distance = voiture.CoordonneesApparition[1] - (uneVoiture.CoordonneesApparition[1] + uneVoiture.Height);
+                        else if (uneVoiture.apparitionToString() == "top")
+                            distance = uneVoiture.CoordonneesApparition[1] - (voiture.CoordonneesApparition[1] + voiture.Height);
+
+                        if (distance < 4 && distance > 0)
+                            return true;
+                    }
+
+                    if (voiture.apparitionToString() == "left")
+                        distance = uneVoiture.CoordonneesApparition[0] - (voiture.CoordonneesApparition[0] + voiture.Width);
+                    else if (voiture.apparitionToString() == "right")
+                        distance = voiture.CoordonneesApparition[0] - (uneVoiture.CoordonneesApparition[0] + uneVoiture.Width);
+                    else if (voiture.apparitionToString() == "bot")
+                        distance = voiture.CoordonneesApparition[1] - (uneVoiture.CoordonneesApparition[1] + uneVoiture.Height);
+                    else if (voiture.apparitionToString() == "top")
+                        distance = uneVoiture.CoordonneesApparition[1] - (voiture.CoordonneesApparition[1] + voiture.Height);
+
+
+                    if (!voiture.Turned && !uneVoiture.Turned && !uneVoiture.Stopped)
+                    {
+                        if (voiture.apparitionToString() == "left" && voiture.Direction == "top" && uneVoiture.apparitionToString() == "right" && distance < 80 && distance > -20 && left <= 369 && left >= 365)
+                            return true;
+                        if (voiture.apparitionToString() == "right" && voiture.Direction == "bot" && uneVoiture.apparitionToString() == "left" && distance < 80 && distance > -20 && left <= 300 && left >= 296)
+                            return true;
+                        if (voiture.apparitionToString() == "bot" && voiture.Direction == "left" && uneVoiture.apparitionToString() == "top" && distance < 80 && distance > -20 && top <= 296 && top >= 292)
+                            return true;
+                        if (voiture.apparitionToString() == "top" && voiture.Direction == "right" && uneVoiture.apparitionToString() == "bot" && distance < 80 && distance > -20 && top <= 360 && top >= 356)
+                            return true;
+                     
+                    }
+
+
+
+
+
+                }
+
+            }
+
+
+            //si aucune raison n'implique l'arret de la voiture, elle ne s'arrête pas...
+            return false;
         }
 
         private void updateVoitures()
         {
 
-            foreach (VoitureAgent voiture in voitureList)
-            {
 
-                string apparition = voiture.apparitionToString();
-                int left = voiture.CoordonneesApparition[0];
-                int top = voiture.CoordonneesApparition[1];
+           // for (int i = 0; i < voitureList.Count - 1; i++ )
 
-
-                if (apparition == "left" && feuG.Fill == Brushes.Red && left == 214)
-                    voiture.Stopped = true;
-                else if (apparition == "right" && feuD.Fill == Brushes.Red && left == 424)
-                    voiture.Stopped = true;
-                else if (apparition == "bot" && feuB.Fill == Brushes.Red && top == 424)
-                    voiture.Stopped = true;
-                else if (apparition == "top" && feuH.Fill == Brushes.Red && top == 214)
-                    voiture.Stopped = true;
-                else
-                    voiture.Stopped = false;
-
-                //si la voiture n'a pas encore tournée
-                if (!voiture.Stopped)
+                for (int i = 0; i <= voitureList.Count - 1; i++)
                 {
-                    if (!voiture.Turned)
-                    {
-                        if (apparition == "top")
-                            voiture.CoordonneesApparition[1] += voiture.Vitesse;
-                        else if (apparition == "bot")
-                            voiture.CoordonneesApparition[1] -= voiture.Vitesse;
-                        else if (apparition == "left")
-                            voiture.CoordonneesApparition[0] += voiture.Vitesse;
-                        else if (apparition == "right")
-                            voiture.CoordonneesApparition[0] -= voiture.Vitesse;
 
-                        if ((apparition == "left" || apparition == "right") && (voiture.Direction != "right" || voiture.Direction != "left"))
+                    VoitureAgent voiture = voitureList[i];
+                    string apparition = voiture.apparitionToString();
+                    int left = voiture.CoordonneesApparition[0];
+                    int top = voiture.CoordonneesApparition[1];
+                    //on détermine si la voiture s'arrête ou non
+                    voiture.Stopped = isStopped(voiture);
+
+
+
+                    //si la voiture n'est pas arrêtée.
+                    if (!voiture.Stopped)
+                    {
+                        //si la voiture n'a pas encore tournée
+                        if (!voiture.Turned)
                         {
-                            if (voiture.Direction == "bot" && left == 300)
-                                voiture.tourner();
-                            else if (voiture.Direction == "top" && left == 370)
-                                voiture.tourner();
+                            // on détermine dans quel sens on fait avancer la voiture
+                            if (apparition == "top")
+                                voiture.CoordonneesApparition[1] += voiture.Vitesse;
+                            else if (apparition == "bot")
+                                voiture.CoordonneesApparition[1] -= voiture.Vitesse;
+                            else if (apparition == "left")
+                                voiture.CoordonneesApparition[0] += voiture.Vitesse;
+                            else if (apparition == "right")
+                                voiture.CoordonneesApparition[0] -= voiture.Vitesse;
+                            //
+
+                            // on détermine si la voiture est dans le carrefour, et donc si c'est le moment de tourner
+                            if ((apparition == "left" || apparition == "right") && (voiture.Direction != "right" || voiture.Direction != "left"))
+                            {
+                                if (voiture.Direction == "bot" && left == 300)
+                                    voiture.tourner();
+                                else if (voiture.Direction == "top" && left == 370)
+                                    voiture.tourner();
+                            }
+                            if ((apparition == "top" || apparition == "bot") && (voiture.Direction != "bot" || voiture.Direction != "top"))
+                            {
+                                if (voiture.Direction == "right" && top == 360)
+                                    voiture.tourner();
+                                else if (voiture.Direction == "left" && top == 296)
+                                    voiture.tourner();
+                            }
+                            //
                         }
-                        if ((apparition == "top" || apparition == "bot") && (voiture.Direction != "bot" || voiture.Direction != "top"))
+                        else
                         {
-                            if (voiture.Direction == "right" && top == 360)
-                                voiture.tourner();
-                            else if (voiture.Direction == "left" && top == 296)
-                                voiture.tourner();
+                            // si la voiture a tourné, il n'y aura plus de changement de direction, donc on fait avancer la voiture dans sa direction, sans se poser de question
+                            if (voiture.Direction == "right")
+                                voiture.CoordonneesApparition[0] += voiture.Vitesse;
+                            if (voiture.Direction == "left")
+                                voiture.CoordonneesApparition[0] -= voiture.Vitesse;
+                            if (voiture.Direction == "bot")
+                                voiture.CoordonneesApparition[1] += voiture.Vitesse;
+                            if (voiture.Direction == "top")
+                                voiture.CoordonneesApparition[1] -= voiture.Vitesse;
                         }
+                    }
 
-                    }
-                    else
-                    {
-                        if (voiture.Direction == "right")
-                            voiture.CoordonneesApparition[0] += voiture.Vitesse;
-                        if (voiture.Direction == "left")
-                            voiture.CoordonneesApparition[0] -= voiture.Vitesse;
-                        if (voiture.Direction == "bot")
-                            voiture.CoordonneesApparition[1] += voiture.Vitesse;
-                        if (voiture.Direction == "top")
-                            voiture.CoordonneesApparition[1] -= voiture.Vitesse;
-                    }
+                    //supprime l'objet voiture s'il est sortie de la route
+                    supprimerVoiture(voiture);
                 }
-
-
-            }
         }
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            dispatcherTimer.Start();
-            feuB.Fill = Brushes.GreenYellow;
-            feuH.Fill = Brushes.GreenYellow;
-            feuG.Fill = Brushes.Red;
-            feuD.Fill = Brushes.Red;
-            feuGActif = false;
-            feuTimer.Start();
-        }
-
-
-       /* private Boolean chercherDanger(VoitureAgent uneVoiture)
-        {
-            foreach (VoitureAgent voiture in voitureList)
-            {
-                if (voiture != uneVoiture){
-                    
-                {
-                    
-                }
-
-            }
-
-            return true;
-        }*/
-        /*
-            Rectangle body;
-            for (int i = 0; i <= routeCanvas.Children.Count - 1; i++)
-            {
-                if (routeCanvas.Children[i] is Rectangle)
-                {
-
-                    if (Canvas.GetLeft(routeCanvas.Children[i]) == 195)
-                    {
-                        body = (Rectangle)routeCanvas.Children[i];
-                        body.Width = 40;
-                        body.Height = 50;
-                        routeCanvas.Children[i] = body;
-                        Canvas.SetTop(routeCanvas.Children[i], Canvas.GetTop(routeCanvas.Children[i]) + 1);
-                    }
-                    else
-                    {
-                        Canvas.SetLeft(routeCanvas.Children[i], Canvas.GetLeft(routeCanvas.Children[i]) + 1);
-                    }
-                }
-            }*/
-
         
         private void drawVoitures()
         {
@@ -332,6 +352,16 @@ namespace TPNote
             {
                 routeCanvas.Children.Remove(rectangle);
             }
+        }
+
+        private void supprimerVoiture(VoitureAgent voiture)
+        {
+
+            if (voiture.CoordonneesApparition[0] > 760 || voiture.CoordonneesApparition[0] < -60 || voiture.CoordonneesApparition[1] < -60 || voiture.CoordonneesApparition[1] > 760)
+            {
+                voitureList.Remove(voiture);
+            }
+
         }
     }
 }
